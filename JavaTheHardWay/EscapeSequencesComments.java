d95497e419479c404d5829ce882782905c37496b
CMoreno04 <cesar.jos.em26@icloud.com>
2019-08-25 20:53:06 -0600

    JTHW Ex 4

---
+++ JavaTheHardWay/EscapeSequencesComments.java
@@ -0,0 +1,18 @@
+public class EscapeSequencesComments {
+  public static void main(String[] args){
+    //This excercice demonstartes escape sequences & comments (like these)!
+    System.out.print("Learn\tJava\n\tthe\nHard\tWay\n\n");
+    System.out.print("\tLearn Java the \"Hard\" way!\n");
+    //System.out.frimp("Learn Java the Hard Way");
+    System.out.print("Hello\n"); //This line prints Hello.
+    System.out.print("Jello\by\n");//This line prints Jelly.
+    /*The quick bron fox jumped over a lazy dog.
+      Quick wafting zephyrs vex bold Jim. */
+    System.out/*testing*/.println("Hard to beleive, eh?");
+    System.out.println("Surprised? /* abcde */ Or what did you excpect?");
+    System.out.println("\\ // -=- \\ //");
+    System.out.println("\\\\ \\\\\\ \\\\\\\\");//it takes 2 to make 1
+    System.out.print("I hope you understand \"escape sequences\"now.\n");
+    // and comments. :)
+  }
+}//Does Compile
