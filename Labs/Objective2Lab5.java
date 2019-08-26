38d1122e4d6655cb894fdb3d0852e7777ade1e0c
CMoreno04 <cesar.jos.em26@icloud.com>
2019-08-25 22:37:22 -0600

    SDPre Obj2Lab5

---
+++ Labs/Objective2Lab5.java
@@ -0,0 +1,12 @@
+public class Objective2Lab5{
+  public static void main ( String[] args){
+    int side1, side2, hypotenuse;
+
+    side1 = 2;
+    side2 = 4;
+    hypotenuse=(side1*side1)+(side2*side2);
+
+    System.out.println("The Hypotenuse of a Right Triangle with sides: "+side1+" and "+side2+" is "+" "+hypotenuse);
+  }
+
+}
