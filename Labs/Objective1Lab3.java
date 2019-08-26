2f3a8e3bf18e9fb3339645de8ebaecb89234b3ff
CMoreno04 <cesar.jos.em26@icloud.com>
2019-08-25 19:45:40 -0600

    SDPre Obj1Lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Obective1Lab3 {
+  public static void main(String[] args){
+    System.out.println("Name: Cesar Moreno");
+    System.out.println("Age: 25");
+    System.out.println("State: Florida");
+  }
+}
