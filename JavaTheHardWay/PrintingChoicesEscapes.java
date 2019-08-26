29e25bdc5b3aa31cf7eb21bf627feadd3d912076
CMoreno04 <cesar.jos.em26@icloud.com>
2019-08-25 20:54:15 -0600

    JTHW Ex 5

---
+++ JavaTheHardWay/PrintingChoicesEscapes.java
@@ -0,0 +1,17 @@
+public class EscapeSequencesComments {
+  public static void main(String[] args){
+
+    System.out.println("Learn\tJava\n\tthe\nHard\tWay\n\n");
+    System.out.println("\tLearn Java the \"Hard\" way!\n");
+
+    System.out.println("Hello\n"); //This line prints Hello.
+    System.out.println("Jello\by\n");//This line prints Jelly.
+
+    System.out.println("Hard to beleive, eh?");
+    System.out.println("Surprised? /* abcde */ Or what did you excpect?");
+    System.out.println("\\ // -=- \\ //");
+    System.out.println("\\\\ \\\\\\ \\\\\\\\");//it takes 2 to make 1
+    System.out.println("I hope you understand \"escape sequences\"now.\n");
+
+  }
+}
