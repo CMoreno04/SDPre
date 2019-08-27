635cf0fd63cbcdbbd347cd0516dbe9659caa7120
CMoreno04 <cesar.jos.em26@icloud.com>
2019-08-27 09:18:47 -0600

    JTHW Ex 21

---
+++ JavaTheHardWay/GenderTitles.java
@@ -0,0 +1,39 @@
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class GenderTitles{
+  public static void main ( String[] args){
+    Scanner keyboard = new Scanner(System.in);
+    String title, first, last, gender, married;
+    int age;
+
+    out.print( "First Name: ");
+    first = keyboard.next();
+    out.print("Last Name: ");
+    last = keyboard.next();
+    out.print("Gender (M/F): ");
+    gender = keyboard.next();
+    out.print(" Age: ");
+    age = keyboard.nextInt();
+
+    if (age<20){
+      title = first;
+    }
+    else {
+      if ( gender.equals("F")){
+        out.print ("Are you married, "+first+"? (Y/N): ");
+        married = keyboard.next();
+        if ( married.equals("Y")){
+          title = " Mrs. ";
+        }
+        else {
+          title = "Ms.";
+        }
+      }
+      else {
+        title = "Mr.";
+      }
+    }
+    out.println( "\n"+title+" "+last);
+  }
+}
