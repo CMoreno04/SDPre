97aaff1ac3796df51f30224dc737c15a3e8885a0
CMoreno04 <cesar.jos.em26@icloud.com>
2019-08-28 15:08:58 -0600

    JTHW Ex 30

--- JavaTheHardWay/RunningTotal.java
+++ JavaTheHardWay/RunningTotal.java
@@ -9,15 +9,18 @@ public class RunningTotal{
     out.print("Type in a unch of values and I'll add them up. ");
     out.println("I'll stop when you type zero.");
 
-    while ( current != 0){
-
+    do{
         out.print("Value: ");
         current = key.nextInt();
+
+        if (current > 0){
         total += current;
         out.println("The total so far is: "+total);
+        }
     }
 
+    while ( current != 0);
+      out.println("The final total is: "+total);
 
-        out.println("The final total is: "+total);
   }
 }
