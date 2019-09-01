33210a1237afdbe24797d4cb603be4d7957a5af2
CMoreno04 <cesar.jos.em26@icloud.com>
2019-09-01 15:09:01 -0600

    SDPre Obj6Lab4

--- Labs/Objective6Lab4.java
+++ Labs/Objective6Lab4.java
@@ -2,13 +2,13 @@ import static java.lang.System.*;
 
 public class Objective6Lab4{
   public static void main(String[] args){
-  int count=21;
+  int count=0;
   int sum=0;
 
-    do {
-      count=count-1;
+    while (count < 20){
+      count=count+1;
       sum=sum+count;
-    }while (count > 0);
+    }
 
     out.println(sum);
 
