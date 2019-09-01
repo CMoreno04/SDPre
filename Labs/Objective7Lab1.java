98c681a8bf099e3db3290ac532a3079abc2797ad
CMoreno04 <cesar.jos.em26@icloud.com>
2019-09-01 16:50:33 -0600

    SDPre Obj7Lab1

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,11 @@
+import static java.lang.System.*;
+
+public class Objective7Lab1{
+  public static void main(String[] args){
+    printName();
+  }
+
+  public static void printName(){
+    out.println("Hello Cesar");
+  }
+}
