ed9758dbf5d16691e5b95721c37cad3d43050e71
CMoreno04 <cesar.jos.em26@icloud.com>
2019-09-01 13:48:52 -0600

    SDPre Obj6Lab2

---
+++ Labs/Objective6Lab2.java
@@ -0,0 +1,12 @@
+import static java.lang.System.*;
+
+public class Objective6Lab2{
+  public static void main(String[] args){
+    int counter =0;
+
+    while (counter < 20){
+      counter = counter +1;
+      out.println(counter);
+    }
+  }
+}
